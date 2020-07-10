4fd204ab9540722453dd03f5e46498731b5522bb
hhstore <hhglory@outlook.com>
2020-07-10 10:58:39 +0800

    debug log

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -7,12 +7,20 @@ import (
 func TestLogger(t *testing.T) {
 	l := NewLogger(WithLevel(TraceLevel))
 	h1 := NewHelper(l).WithFields(map[string]interface{}{"key1": "val1"})
-	h1.Trace("trace_msg1")
+	h1.Debug("debug_msg1")
 	h1.Warn("warn_msg1")
+	h1.Error("error_msg1")
+	h1.Trace("trace_msg1")
 
 	h2 := NewHelper(l).WithFields(map[string]interface{}{"key2": "val2"})
 	h2.Trace("trace_msg2")
 	h2.Warn("warn_msg2")
 
 	l.Fields(map[string]interface{}{"key3": "val4"}).Log(InfoLevel, "test_msg")
+
+	//
+	h2.Debugf("debugf %v", "debug")
+	h2.Errorf("errorf %v", "error")
+
 }
+
